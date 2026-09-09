723c6aa82bbf119df715b3c1941797013a4eb41a
Canelo88 <andresgonzalez1988@hotmail.com>
2026-09-09 17:42:31 +0000

    Se agrega Ejercio de Calculadora de Canelo88

---
+++ src/ejercicio/java/canelo/Ejercicio_02_Calculadora.java
@@ -0,0 +1,37 @@
+package ejercicio.java.canelo;
+
+public class Ejercicio_02_Calculadora {
+
+    public static void main(String[] args) {
+        int vNumero1 = 20;
+        int vNumero2 = 5;
+
+        System.out.println("==============================");
+        System.out.println("CALCULADORA");
+        System.out.println("==============================");
+        System.out.println("Número 1: " + vNumero1);
+        System.out.println("Número 2: " + vNumero2);
+        System.out.println("------------------------------");
+        System.out.println("Suma: " + Suma(vNumero1, vNumero2));
+        System.out.println("Resta: " + Resta(vNumero1, vNumero2));
+        System.out.println("Multiplicación: " + Multiplicacion(vNumero1, vNumero2));
+        System.out.println("División: " + Division(vNumero1, vNumero2));
+        System.out.println("==============================");
+    }
+
+    public static int Suma(int pVal1, int pVal2) {
+        return pVal1 + pVal2;
+    }
+
+    public static int Resta(int pVal1, int pVal2) {
+        return pVal1 - pVal2;
+    }
+
+    public static int Multiplicacion(int pVal1, int pVal2) {
+        return pVal1 * pVal2;
+    }
+
+    public static double Division(int pVal1, int pVal2) {
+        return pVal1 / pVal2;
+    }
+}
