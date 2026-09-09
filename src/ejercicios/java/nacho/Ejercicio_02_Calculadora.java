b4afedc155a25c90fea1b7a122466e69699ac28c
joseignaciogarciarivas <jgarcia70317@ufide.ac.cr>
2026-09-09 23:50:50 +0000

    Estoy cambiando que los valores no ssean fijos y se puedan ingresar

--- src/ejercicios/java/nacho/Ejercicio_02_Calculadora.java
+++ src/ejercicios/java/nacho/Ejercicio_02_Calculadora.java
@@ -1,12 +1,26 @@
 package ejercicios.java.nacho;
+import java.util.Scanner;
 
 public class Ejercicio_02_Calculadora {
 
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.println("Hola bienvenido a la Calculadora");
+        System.out.println("Ingrese el valor del primer numero");
+        int numero1 = scanner.nextInt();
+        System.out.println("Ingrese el valor del segundo numero");
+        int numero2 = scanner.nextInt();
+        
+
+        
+
+    }
+    public static void MenuPrincipal(){
 
         // Inicializacion de variables
-        int numero1 = 21;
-        int numero2 = 5;
+        int numero1 = 0;
+        int numero2 = 0;
 
         System.out.println("==========================");
         System.out.println("     CALCULADORA NACHO    ");
@@ -19,9 +33,7 @@ public class Ejercicio_02_Calculadora {
         System.out.println("Multiplicacion: " + Multiplicacion(numero1, numero2));
         System.out.println("Division: " + Division(numero1, numero2));
         System.out.println("==========================");
-
     }
-
     // Funciones
     public static int Suma(int pNumero1, int pNumero2) {
 
