8f518dea59337e3c55d813df7f2ed8b92847f589
Andrés González García <andresgonzalez1988@hotmail.com>
2026-09-09 10:24:47 -0600

    Agregar codigo Andres

---
+++ src/ejercicio/java/canelo/Ejercicio_01.java
@@ -0,0 +1,31 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ejercicio.java.canelo;
+
+/**
+ *
+ * @author andresgonzalezgarcia
+ */
+public class Ejercicio_01 {
+     
+    public static void main(String[] args) {
+        Presentacion();
+    }
+    
+    public static void Presentacion(){
+        String nombre = "Andrés González García";
+        int edad = 38;
+        String ciudad = "Heredia";
+        
+        System.out.println("=========================");
+        System.out.println("MI PRESENTACIÓN");
+        System.out.println("=========================");
+        System.out.println("Nombre: " + nombre);
+        System.out.println("Edad: " + edad);
+        System.out.println("Ciudad: " + ciudad);
+        System.out.println("Estoy aprendiendo Java");
+        
+    }
+}
