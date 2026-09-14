7e0485f4231e361d9706787ef89a375d22666e1c
Canelo88 <andresgonzalez1988@hotmail.com>
2026-09-14 17:02:13 +0000

    Ejercicio 3 - Calculadora con Static

--- src/ejercicios_canelo/ejercicio_3/Calculadora.java
+++ src/ejercicios_canelo/ejercicio_3/Calculadora.java
@@ -1,9 +1,22 @@
+import model.Procesos;
+
 public class Calculadora {
 
     public static void main(String[] args) {
-        int val1 = 0;
-        int val2 = 0;
+        int vNumero1 = 21;
+        int vNumero2 = 5;
 
+        System.out.println("==============================");
+        System.out.println("CALCULADORA - Canelo88 v2");
+        System.out.println("==============================");
+        System.out.println("Número 1: " + vNumero1);
+        System.out.println("Número 2: " + vNumero2);
+        System.out.println("------------------------------");
+        System.out.println("Suma: " + Procesos.Suma(vNumero1, vNumero2));
+        System.out.println("Resta: " + Procesos.Resta(vNumero1, vNumero2));
+        System.out.println("Multiplicación: " + Procesos.Multiplicacion(vNumero1, vNumero2));
+        System.out.println("División: " + Procesos.Division(vNumero1, vNumero2));
+        System.out.println("==============================");
         
     }
 }
--- src/ejercicios_canelo/ejercicio_3/model/Procesos.java
+++ src/ejercicios_canelo/ejercicio_3/model/Procesos.java
@@ -1,5 +1,20 @@
-package ejercicio.java.canelo.ejercicio_3.model;
+package model;
 
 public class Procesos {
-    
+
+    public static int Suma(int pVal1, int pVal2){
+        return pVal1 + pVal2;
+    }
+
+    public static int Resta(int pVal1, int pVal2){
+        return pVal1 - pVal2;
+    }
+
+    public static int Multiplicacion(int pVal1, int pVal2){
+        return pVal1 * pVal2;
+    }
+
+    public static double Division(int pVal1, int pVal2){
+        return (double) pVal1 / pVal2;
+    }
 }
