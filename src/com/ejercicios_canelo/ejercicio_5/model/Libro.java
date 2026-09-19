2a5aebd14e8ee8fb49ea4ce76d924e40adccd406
Canelo88 <andresgonzalez1988@hotmail.com>
2026-09-19 16:48:00 +0000

    Se realizan mejoras visuales en ejericio 5 canelo clase Libro

--- src/com/ejercicios_canelo/ejercicio_5/model/Libro.java
+++ src/com/ejercicios_canelo/ejercicio_5/model/Libro.java
@@ -1,29 +1,32 @@
 package com.ejercicios_canelo.ejercicio_5.model;
 
-public class Libro{
+public class Libro {
 
     private String titulo;
     private String isbn;
     private Autor autor;
     private static int contador;
 
-    public Libro(String pTitulo, String pIsbn, Autor pAutor){
+    // Constructor con todos los valrores
+    public Libro(String pTitulo, String pIsbn, Autor pAutor) {
         this.titulo = pTitulo;
         this.isbn = pIsbn;
         this.autor = pAutor;
         this.contador++;
     }
 
-    public static int getContador(){
+    // Gets & Sets
+    public static int getContador() {
         return contador;
     }
 
-    @Override 
-    public String toString(){
+    // Se reescribe metod toString
+    @Override
+    public String toString() {
         StringBuilder sb = new StringBuilder();
-        sb.append(titulo);
-        sb.append(isbn);
+        sb.append("Titulo = " + titulo);
+        sb.append("Isbn = " + isbn);
         sb.append(autor.toString());
-        return  sb.toString();
+        return sb.toString();
     }
 }
